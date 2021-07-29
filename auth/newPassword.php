7d0928b422fa609d94fa0f69e6f88fd4d7bcb10e
Abdulmutalib Amoka <amokamutalibfut@gmail.com>
2021-07-29 23:33:22 +0100

    added change password

--- auth/newPassword.php
+++ auth/newPassword.php
@@ -23,4 +23,37 @@
     //Get form data
     $data = json_decode(file_get_contents("php://input"));
     $returnData = [];
+    
+    if($_SERVER["REQUEST_METHOD"] != "POST"){
+        $returnData = messg(0, 404, "Page Not Found");
+    }elseif(
+        !isset($data->userId)
+    || !isset($data->password)
+    || empty(trim($data->userId))
+    || empty(trim($data->password))
+    ){
+        $returnData = messg(0, 422, 'All required field was not provided!');
+    }else{
+        try {
+            //code...
+            $userId = mysqli_real_escape_string($dbConnect, trim($data->userId));
+            $password = mysqli_real_escape_string($dbConnect, trim($data->password));
+            if(strlen($password) < 8){
+                throw("password length must not be less than eight");
+            }else{
+                $hassPassword = password_hash($password, PASSWORD_DEFAULT);
+                $updateQry = "UPDATE `users` SET `password`='$hassPassword' WHERE `id`='$userId'";
+                $updateResult = $dbConnect->query($updateQry);
+                if($updateResult){
+                    //Password change successful
+                    $returnData = messg(1, 200, 'Success! your password had been updated');
+                }else{
+                    throw('Oops! something happened while trying to update your password, please try again or contact admin');
+                }
+            }
+        } catch (Exception $e) {
+            //throw $e;
+            $returnData = messg(0, 400, $e->getMessage());
+        }
+    }
 ?>
