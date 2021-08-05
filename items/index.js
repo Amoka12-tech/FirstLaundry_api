3e6dc50643c4739cb07f381b7e946cf109c6b942
Abdulmutalib Amoka <amokamutalibfut@gmail.com>
2021-08-06 00:34:39 +0100

    add item images

---
+++ items/index.js
@@ -0,0 +1,5 @@
+export const TShirt = './Tshirt.png';
+export const Shirt = './Shirt.png';
+export const Sleveless = './Sleveless.png';
+export const Trouser = './Trouser.png';
+export const upDown = './MGown.png';
